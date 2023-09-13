97c498aebe90c6ee6e66e2344aac3399f2e435c9
abdul kadar khan <monukadar12345@gmail.com>
2023-09-13 21:52:10 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,14 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
